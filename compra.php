21b0e90273237d6de0ca2a19af260fe72fc96299
Miguel Vazquez <miguelornelas54@gmail.com>
2025-10-16 23:32:43 -0600

    Correcion de errores

--- compra.php
+++ compra.php
@@ -1,89 +1,88 @@
 <?php
-session_start();
-include 'conexion.php';
-if (!isset($_SESSION['ID_Usuario'])) {
-<<<<<<< HEAD
-    header("Location: ./");
-=======
-    header("Location: index_login.php");
->>>>>>> 5eed61a4d4e85571766fd97c4bd216cc98152e4a
-    exit();
-}
-$usuario_id = $_SESSION['ID_Usuario'];
-
-$sql = "SELECT * FROM Carrito_Compra WHERE ID_Usuario = $usuario_id";
-$result = $conn->query($sql);
-if ($result->num_rows == 0) {
-    header("Location: carrito.php");
-    exit();
-}
-while ($row = $result->fetch_assoc()) {
-    $productos[] = $row['ID_Producto'];
-    $cantidades[] = $row['Cantidad'];
-}
-
-foreach ($productos as $index => $value) {
-    $sql = "SELECT * FROM Productos WHERE ID_Producto = $value";
-    $result = $conn->query($sql);
-    $producto = $result->fetch_assoc();
-    $stock = $producto['Stock'];
-    $precios[$index] = $producto['Precio'];
-    if ($stock < $cantidades[$index]) {
-        echo '<script> alert("No hay stock suficiente para alguno de los productos"); window.location.href = "carrito.php"; </script>';
+    session_start();
+    include 'conexion.php';
+    if (!isset($_SESSION['ID_Usuario'])) {
+        header("Location: ./");
         exit();
     }
-    $sql = $stock == $cantidades[$index] ?
-    "DELETE FROM Productos WHERE ID_Producto = $value" :
-    "UPDATE Productos SET Stock = $stock - $cantidades[$index] WHERE ID_Producto = $value";
+    $usuario_id = $_SESSION['ID_Usuario'];
+
+    $sql = "SELECT * FROM Carrito_Compra WHERE ID_Usuario = $usuario_id";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0) {
+        header("Location: carrito.php");
+        exit();
+    }
+
+    $productos = [];
+    $cantidades = [];
+    $precios = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $productos[] = $row['ID_Producto'];
+        $cantidades[] = $row['Cantidad'];
+    }
+
+    foreach ($productos as $index => $value) {
+        $sql = "SELECT * FROM Productos WHERE ID_Producto = $value";
+        $result = $conn->query($sql);
+        $producto = $result->fetch_assoc();
+        $stock = $producto['Stock'];
+        $precios[$index] = $producto['Precio'];
+        if ($stock < $cantidades[$index]) {
+            echo '<script> alert("No hay stock suficiente para alguno de los productos"); window.location.href = "carrito.php"; </script>';
+            exit();
+        }
+        $sql = $stock == $cantidades[$index] ?
+        "UPDATE Productos SET Stock = 0 WHERE ID_Producto = $value" :
+        "UPDATE Productos SET Stock = $stock - $cantidades[$index] WHERE ID_Producto = $value";
+        $conn->query($sql);
+    }
+
+    $total = $_POST['total'];
+    $metodo_pago = $_POST['metodo_pago'];
+    $direccion = $_POST['direccion'];
+
+    if (!$metodo_pago) {
+        echo '<script> alert("Selecciona un metodo de pago");</script>';
+        echo '<script> window.location.href = "carrito.php";</script>';
+        exit();
+    }
+
+    if (!$direccion) {
+        echo '<script> alert("Selecciona una direccion de entrega");</script>';
+        echo '<script> window.location.href = "carrito.php";</script>';
+        exit();
+    }
+
+    $sql = "SELECT * FROM Direcciones_Entrega WHERE ID_Direccion_Entrega = $direccion";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $direccion = $result->fetch_assoc();
+        $calle = $direccion['Calle'];
+        $numeroInterior = isset($direccion['Num_Interior']) ? $direccion['Num_Interior'] : null;
+        $numeroExterior = $direccion['Num_Exterior'];
+        $fracc = $direccion['Fraccionamiento_Colonia'];
+        $codigoPostal = $direccion['Codigo_Postal'];
+        $estado = $direccion['Estado'];
+        $municipio = $direccion['Municipio'];
+        $localidad = $direccion['Localidad'];
+    }
+
+
+    $sql = "INSERT INTO Venta (Total, ID_Metodo_Pago, ID_Usuario, Calle, Num_Interior, Num_Exterior, Fraccionamiento_Colonia, Codigo_Postal, Estado, Municipio, Localidad) VALUES ($total, $metodo_pago, $usuario_id, '$calle'," . ($numeroInterior ? "'$numeroInterior'" : "NULL") . ", '$numeroExterior', '$fracc', '$codigoPostal', '$estado', '$municipio', '$localidad');";
     $conn->query($sql);
-}
+    $venta_id = $conn->insert_id;
 
-$total = $_POST['total'];
-$metodo_pago = $_POST['metodo_pago'];
-$direccion = $_POST['direccion'];
+    foreach ($productos as $index => $value) {
+        $sql = "INSERT INTO Detalle_Venta (ID_Venta, ID_Producto, Cantidad, Precio) VALUES ($venta_id, $value, $cantidades[$index], $precios[$index]);";
+        $conn->query($sql);
+    }
 
-<<<<<<< HEAD
-if (!$metodo_pago) {
-    echo '<script> alert("Selecciona un metodo de pago");</script>';
-    echo '<script> window.location.href = "carrito.php";</script>';
-    exit();
-}
-
-if (!$direccion) {
-    echo '<script> alert("Selecciona una direccion de entrega");</script>';
-    echo '<script> window.location.href = "carrito.php";</script>';
-    exit();
-}
-
-=======
->>>>>>> 5eed61a4d4e85571766fd97c4bd216cc98152e4a
-$sql = "SELECT * FROM Direcciones_Entrega WHERE ID_Direccion_Entrega = $direccion";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    $direccion = $result->fetch_assoc();
-    $calle = $direccion['Calle'];
-    $numeroInterior = $direccion['Num_Interior'];
-    $numeroExterior = $direccion['Num_Exterior'];
-    $fracc = $direccion['Fraccionamiento_Colonia'];
-    $codigoPostal = $direccion['Codigo_Postal'];
-    $estado = $direccion['Estado'];
-    $municipio = $direccion['Municipio'];
-    $localidad = $direccion['Localidad'];
-}
-
-
-$sql = "INSERT INTO Venta (Total, ID_Metodo_Pago, ID_Usuario, Calle, Num_Interior, Num_Exterior, Fraccionamiento_Colonia, Codigo_Postal, Estado, Municipio, Localidad) VALUES ($total, $metodo_pago, $usuario_id, '$calle'," . ($numeroInterior ? "'$numeroInterior'" : "NULL") . ", '$numeroExterior', '$fracc', '$codigoPostal', '$estado', '$municipio', '$localidad');";
-$conn->query($sql);
-$venta_id = $conn->insert_id;
-
-foreach ($productos as $index => $value) {
-    $sql = "INSERT INTO Detalle_Venta (ID_Venta, ID_Producto, Cantidad, Precio) VALUES ($venta_id, $value, $cantidades[$index], $precios[$index]);";
+    $sql = "DELETE FROM Carrito_Compra WHERE ID_Usuario = $usuario_id";
     $conn->query($sql);
-}
 
-$sql = "DELETE FROM Carrito_Compra WHERE ID_Usuario = $usuario_id";
-$conn->query($sql);
-
-echo '<script> alert("Compra realizada con exito");</script>';
-echo '<script> window.location.href = "ventas.php";</script>';
+    echo '<script> alert("Compra realizada con exito");</script>';
+    echo '<script> window.location.href = "ventas.php";</script>';
 ?>
